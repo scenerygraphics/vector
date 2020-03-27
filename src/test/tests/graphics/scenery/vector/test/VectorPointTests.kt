a22c2eed276edab7ca0f838fdc1832bf8c21b6a9
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2020-03-27 12:16:34 -0400

    Test use of VectorPoint3f with imglib2 code

--- src/test/tests/graphics/scenery/vector/test/VectorPointTests.kt
+++ src/test/tests/graphics/scenery/vector/test/VectorPointTests.kt
@@ -2,9 +2,13 @@ package graphics.scenery.vector.test
 
 import graphics.scenery.vector.VectorPoint3f
 import graphics.scenery.vector.VectorPoint4f
+import net.imglib2.img.array.ArrayImgs
+import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory
+import net.imglib2.type.numeric.integer.UnsignedByteType
+import net.imglib2.view.Views
 import org.junit.Test
+import kotlin.random.Random
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 /**
  * Tests for [VectorPoint3f] and [VectorPoint4f] classes.
@@ -30,7 +34,7 @@ class VectorPointTests {
         assertEquals(2f,vmul.x(),"mul test")
 
         val vdiv = v / v2
-        assertEquals(0.5f,vdiv.x(),"mul test")
+        assertEquals(0.5f,vdiv.x(),"div test")
     }
 
 
@@ -50,6 +54,29 @@ class VectorPointTests {
         assertEquals(2f,vmul.x(),"mul test")
 
         val vdiv = v / v2
-        assertEquals(0.5f,vdiv.x(),"mul test")
+        assertEquals(0.5f,vdiv.x(),"div test")
+    }
+
+    @Test
+    fun testImgLib() {
+        println("Testing VectorPoint3f methods")
+        val v = VectorPoint3f(1.0f, 0.0f, 0.0f)
+
+        val rng = Random(17)
+
+        val dim = 10L
+        val img = ArrayImgs.unsignedBytes(dim, dim, dim)
+        val cur = img.cursor()
+        while( cur.hasNext() ) {
+            cur.fwd()
+            cur.get().set(rng.nextInt(255))
+        }
+
+        val realImg = Views.interpolate(img, NLinearInterpolatorFactory<UnsignedByteType>())
+        val realImgAccess = realImg.realRandomAccess()
+
+        realImgAccess.setPosition(v)
+
+        assertEquals(123, realImgAccess.get().get(), "RealLocalizable access")
     }
 }
